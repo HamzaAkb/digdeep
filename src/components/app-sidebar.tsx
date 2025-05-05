a23f1258c6062ef38c254472597a26a28b48fc76
HamzaAkb <hamzawakbar@gmail.com>
2025-05-05 20:03:10 +0500

    feat: Refactor AppSidebar component to include header, footer, and session group

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,11 +1,39 @@
-import { Sidebar, SidebarContent, SidebarFooter } from '@/components/ui/sidebar';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+} from '@/components/ui/sidebar'
 import { ModeToggle } from './mode-toggle'
+import { AlignHorizontalSpaceBetween, Plus } from 'lucide-react'
+import { Separator } from './ui/separator'
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
-      <SidebarFooter><ModeToggle /></SidebarFooter>
+      <SidebarHeader>
+        <div className='flex items-center gap-2 ml-1 my-2'>
+          <AlignHorizontalSpaceBetween />
+          <span className='font-semibold text-lg'>Dig Deep</span>
+        </div>
+        <Separator />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Sessions</SidebarGroupLabel>
+          <SidebarGroupAction>
+            <Plus /> <span className='sr-only'>Create Session</span>
+          </SidebarGroupAction>
+          <SidebarGroupContent></SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <ModeToggle />
+      </SidebarFooter>
     </Sidebar>
   )
 }

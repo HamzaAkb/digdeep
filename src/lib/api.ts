b958e26d8c7a74fe662dfd10ecca8e581354c916
HamzaAkb <hamzawakbar@gmail.com>
2025-05-07 15:25:21 +0500

    feat: Add axios API client with request and response interceptors

---
+++ src/lib/api.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const API_BASE = import.meta.env.VITE_API_URL
+
+const api = axios.create({
+    baseURL: `${API_BASE}/api/v1`,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+})
+
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('access_token')
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    err => Promise.reject(err)
+)
+
+api.interceptors.response.use(
+    resp => resp,
+    err => {
+        if (err.response?.status === 401) {
+            window.location.href = '/auth'
+        }
+        return Promise.reject(err)
+    }
+)
+
+export default api
